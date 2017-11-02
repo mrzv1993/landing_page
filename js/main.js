c89f63891575ab77491d0f61fd0b161fc1e73c16
mrzvGit <xxmrzv@gmail.com>
2017-11-02 22:06:59 +1000

    form_login

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var login = document.querySelector(".login");
+var popup = document.querySelector(".popup");
+
+login.addEventListener("click", function() {
+popup.classList.add("popup-show");
+        });
+
+
+
